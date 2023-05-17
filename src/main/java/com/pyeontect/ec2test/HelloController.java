78a749077a302f4f35d6f9304f1e60e70cb56a96
Ganghee Lee <gh_5226@naver.com>
2023-05-17 23:20:03 +0900

    test:ec2 동작이 제대로 되는지 확인하기 위해서 간단한 controller 추가

---
+++ src/main/java/com/pyeontect/ec2test/HelloController.java
@@ -0,0 +1,12 @@
+package com.pyeontect.ec2test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "테스트가 정상적으로 출력됩니다.";
+    }
+}
