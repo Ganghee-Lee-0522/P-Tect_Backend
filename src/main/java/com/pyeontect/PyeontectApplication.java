4ae45004dd605f50a22cbc7681877c7a5933283b
Ganghee Lee <gh_5226@naver.com>
2023-05-17 17:49:18 +0900

    chore: properties 파일 설정

--- src/main/java/com/pyeontect/PyeontectApplication.java
+++ src/main/java/com/pyeontect/PyeontectApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PyeontectApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.properties,"
+			+ "src/main/resources/application-db.properties"
+			+ "src/main/resources/application-jwt.properties";
+
 	public static void main(String[] args) {
 		SpringApplication.run(PyeontectApplication.class, args);
 	}
